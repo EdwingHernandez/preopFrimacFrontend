95fdaf90aa83dc64ec75d5cf5d86ed5590a4da97
EdwingHernandez <124322869+EdwingHernandez@users.noreply.github.com>
2025-03-03 18:41:52 -0500

    style :lipstick: Ajuste media query mobile

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,9 @@ import LoginSurvey from "./LoginSurvey";
 
 const App: React.FC = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 p-6">
-      <div className="bg-white shadow-lg rounded-lg p-6 max-w-lg w-full">
+    <div>
         {/* <h1 className="text-2xl font-bold text-center mb-4">Encuesta</h1> */}
         <LoginSurvey />
-      </div>
     </div>
   );
 };
